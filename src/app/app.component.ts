239b6e6ea8f75f73844d0d731c4daa642d24371a
sfujimoto <unicoshun@gmail.com>
2016-10-01 18:06:24 +0900

    ~28 @Input and @Output binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <h1>Root Component</h1>
+    <fa-databinding></fa-databinding>
+  `
 })
 export class AppComponent {
   title = 'app works!!!changed!';
